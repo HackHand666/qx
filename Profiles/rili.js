6f899920bfd7d87f6dbefb9d90ad86ae0add2d5d
HackHand666 <65205555+HackHand666@users.noreply.github.com>
2022-08-05 09:16:40 +0800

    Delete rili.js

--- Profiles/rili.js
+++
@@ -1,8 +0,0 @@
-[MITM]
-hostname = wnl28.jiemengjia.com
-
-
-let obj = JSON.parse($response.body);
-obj.data.is_vip = 1;
-obj.data.end_time = 2031-09-17 11:11:11;
-$done({body: JSON.stringify(obj)});
